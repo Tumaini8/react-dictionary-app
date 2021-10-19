f67791bdbcc96424724500fd0c3cef943d5947d8
Tumaini8 <lutufyotumaini6@gmail.com>
2021-10-19 14:00:24 +0300

    initial  commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Logo from "./Logo.jpg";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={Logo} className="App-logo img-fluid" alt="logo" />
+      </header>
+    </div>
+  );
+}
+
+export default App;
